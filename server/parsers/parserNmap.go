d781bfdc950634b94b5fe8de9ae26f0cb04896f7
nodauf <nodauf@krutt.org>
2021-05-22 21:46:26 +0200

    FIx some issue on nmap parser

--- server/parsers/parserNmap.go
+++ server/parsers/parserNmap.go
@@ -1,6 +1,7 @@
 package parsers
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"strconv"
@@ -13,6 +14,7 @@ import (
 
 func (parse Parser) ParseNmap(taskName, cmdline, stdout, stderr string) bool {
 	fmt.Println("parse nmap")
+	fmt.Println(stdout)
 	var nmap modelsParsers.Nmaprun
 	var portList []string
 	// we unmarshal our byteArray which contains our
@@ -29,38 +31,43 @@ func (parse Parser) ParseNmap(taskName, cmdline, stdout, stderr string) bool {
 			portList = append(portList, strconv.Itoa(port.Port))
 		}
 
-	} else {
-		host.Address = nmap.Host.Address.Addr
-		if len(nmap.Host.Hostnames.Hostname) > 0 {
-			var domain modelsDatabases.Domain
-			domain.Domain = nmap.Host.Hostnames.Hostname[0].Name
-			host.Domain = append(host.Domain, domain)
-		}
 	}
+	host.Address = nmap.Host.Address.Addr
 
 	var ports []modelsDatabases.Port
 	for _, portNmap := range nmap.Host.Ports.Port {
+		if portNmap.State.State == "open" {
+			var port modelsDatabases.Port
+			// If we retrieve the object from the database it may already have some ports
+			if index, ok := utils.StringInSlice(portNmap.Portid, portList); ok {
+				port = host.Ports[index]
+			} else {
 
-		var port modelsDatabases.Port
-		// If we retrieve the object from the database it may already have some ports
-		if index, ok := utils.StringInSlice(portNmap.Portid, portList); ok {
-			port = host.Ports[index]
-		} else {
+				port.Port, _ = strconv.Atoi(portNmap.Portid)
+				port.Service = portNmap.Service.Name
+				port.Version = portNmap.Service.Version
+			}
 
-			port.Port, _ = strconv.Atoi(portNmap.Portid)
-			port.Service = portNmap.Service.Name
-			port.Version = portNmap.Service.Version
+			var portComment modelsDatabases.PortComment
+			portComment.CommandOutput = portNmap.Script.Output
+			portComment.Task = taskName
+			port.PortComment = append(port.PortComment, portComment)
+
+			ports = append(ports, port)
 		}
-
-		var portComment modelsDatabases.PortComment
-		portComment.CommandOutput = portNmap.Script.Output
-		portComment.Task = taskName
-		port.PortComment = append(port.PortComment, portComment)
-
-		ports = append(ports, port)
 	}
 	host.Ports = append(host.Ports, ports...)
+	t, _ := json.MarshalIndent(host, "", " ")
+	fmt.Println(string(t))
+
+	// Workaround attach an existing domain if the host does not exist does not work
 	db.AddOrUpdateHost(&host)
+	if len(nmap.Host.Hostnames.Hostname) > 0 {
+		var domain modelsDatabases.Domain
+		domain.Domain = nmap.Host.Hostnames.Hostname[0].Name
+		host.Domain = append(host.Domain, domain)
+		db.AddOrUpdateHost(&host)
+	}
 
 	//fmt.Println("stdout: " + stdout)
 	//fmt.Println("stderr: " + stderr)
