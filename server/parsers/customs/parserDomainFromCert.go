dca5f1a55d670460e55350747fd8446e196ebac2
nodauf <nodauf@krutt.org>
2021-05-25 18:13:43 +0200

    custom task to extract hostname in SNI

--- server/parsers/customs/parserDomainFromCert.go
+++ server/parsers/customs/parserDomainFromCert.go
@@ -1,9 +1,31 @@
 package parsersCustoms
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/nodauf/ReconFramwork/server/db"
+	modelsDatabases "github.com/nodauf/ReconFramwork/server/models/database"
+)
 
 func (parse Parser) ParseDomainFromCert(taskName, cmdline, stdout, stderr string) bool {
-	fmt.Println(stdout)
+	hostnameWithIP := make(map[string]string)
+	// Convert the string to a map to be sure there is no duplicate host
+	for _, line := range strings.Split(stdout, "\n") {
+		hostname := strings.Split(line, ":")[0]
+		ip := strings.Split(line, ":")[1]
+		hostnameWithIP[hostname] = ip
+	}
+	for hostname, ip := range hostnameWithIP {
+		var domain modelsDatabases.Domain
+		var host modelsDatabases.Host
+
+		host.Address = ip
+		domain.Domain = hostname
+		domain.Host = append(domain.Host, host)
+
+		db.AddDomain(domain)
+	}
 	fmt.Println("Parse DomainFromCert")
 	return true
 }
--- tasks/custom/DomainFromCert.go
+++ tasks/custom/DomainFromCert.go
@@ -1,8 +1,51 @@
 package customTasks
 
-import "fmt"
+import (
+	"crypto/tls"
+	"net"
+	"regexp"
+)
 
 func DomainFromCert(taskName, cmd string) (string, string, string, string, error) {
-	fmt.Println("Custom task")
-	return taskName, cmd, "stdout.String()", "stderr.String()", nil
+	var certificates string
+
+	conn, err := tls.Dial("tcp", cmd, &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		return taskName, cmd, "", "", err
+	}
+	state := conn.ConnectionState()
+	if err != nil {
+		return taskName, cmd, "", "", err
+	}
+
+	conn.Close()
+
+	// Only analyze the website's certificate
+	websiteCerts := state.PeerCertificates[0]
+	regex, err := regexp.Compile("^([a-zA-Z0-9]{1}[a-zA-Z0-9_-]{0,62}){1}(\\.[a-zA-Z0-9_]{1}[a-zA-Z0-9_-]{0,62})*?$")
+	if regex.MatchString(websiteCerts.Subject.CommonName) {
+		certificates += websiteCerts.Subject.CommonName + ":" + resolveHostname(websiteCerts.Subject.CommonName)
+	}
+
+	for _, websiteCert := range websiteCerts.DNSNames {
+		if regex.MatchString(websiteCert) {
+			certificates += "\n" + websiteCert + ":" + resolveHostname(websiteCert)
+		}
+
+	}
+	return taskName, cmd, certificates, "", nil
+}
+
+func resolveHostname(hostname string) string {
+	var ip string
+	addrs, err := net.LookupIP(hostname)
+	if err == nil {
+		for _, addr := range addrs {
+			ip += addr.String() + ","
+		}
+	}
+	// Return ip except the last character which is a comma
+	return ip[:len(ip)-1]
 }
