6af71a1c36a00cab0ed3970206d878c02aed063c
nodauf <nodauf@krutt.org>
2021-05-26 21:48:15 +0200

    Add description for the tasks

--- server/models/workflow.go
+++ server/models/workflow.go
@@ -1,10 +1,11 @@
 package models
 
 type Workflow struct {
-	Name     string         `yaml:"name"`
-	Tags     []string       `yaml:"tags,omitempty"`
-	Commands []string       `yaml:"commands,omitempty"`
-	Options  WorkflowOption `yaml:"options,omitempty"`
+	Name        string         `yaml:"name"`
+	Description string         `yaml:"description"`
+	Tags        []string       `yaml:"tags,omitempty"`
+	Commands    []string       `yaml:"commands,omitempty"`
+	Options     WorkflowOption `yaml:"options,omitempty"`
 }
 
 type WorkflowOption struct {
