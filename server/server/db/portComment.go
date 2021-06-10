7a4353fff3747d198892865618bd6f2401b751d4
nodauf <nodauf@krutt.org>
2021-06-10 16:56:16 +0200

    Fix issue where ip wasn't used

--- server/server/db/portComment.go
+++ server/server/db/portComment.go
@@ -8,6 +8,7 @@ func GetPortComment(ip, port, task string) string {
 	var host modelsDatabases.Host
 	db.Preload("Ports", "port = ?", port).
 		Preload("Ports.PortComment", "task = ?", task).
+		Where("address = ?", ip).
 		First(&host)
 	return host.Ports[0].PortComment[0].CommandOutput
 }
